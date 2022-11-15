64b8daa1c01488c98a4fe615e7b39fdc3753d1df
Terry Hearst <thearst3rd@gmail.com>
2022-11-14 21:33:58 -0500

    Read configuration from file

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 // PlatinumQuest replay porfolio
 // by thearst3rd
 
-const PORT = 3000
-
 const express = require("express")
 const app = express()
 const path = require("path")
+const fs = require("fs")
+
+// Default configuration. These properties can be overrided by creating a file "config.json"
+let config = {
+	port: 3000,
+	title: "PlatinumQuest Replay Portfolio",
+}
+
+try {
+	let newConfig = JSON.parse(fs.readFileSync(path.join(__dirname, "config.json")).toString())
+	for (const key in newConfig) {
+		config[key] = newConfig[key]
+	}
+}
+catch (err) {
+	// no big deal, we just roll with the default
+}
 
 app.use(express.static(path.join(__dirname, "public")))
 
-const server = app.listen(PORT, () =>
-{
+const server = app.listen(config.port, () => {
 	console.log("Listening on port " + server.address().port)
 })
